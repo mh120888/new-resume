33cf515c267395cbd62c4d857c87ccef67901070
Matt Higgins <matthew.alan.higgins@gmail.com>
2014-07-28 19:58:56 -0700

    fixes other typos in mailer

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -2,7 +2,7 @@ class MessageMailer < ActionMailer::Base
   # default from: "from@example.com"
   def mail_me(args)
     @email = args[:email]
-    @name = arg[:name]
+    @name = args[:name]
     @body = args[:body]
     mail(to: 'matthew.alan.higgins@gmail.com', subject: 'Message from Resume Site', from: @email, name: @name)
   end
